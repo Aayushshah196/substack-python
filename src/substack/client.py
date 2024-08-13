ac7b7c04ece79a0dd359caad4ca78c952fe067c3
Aayushshah196 <aayushshah.kanu@docsumo.com>
2024-08-13 08:25:40 +0545

    moved substack inside folder src

--- src/substack/client.py
+++ src/substack/client.py
@@ -1,3 +1,6 @@
+from .api.posts import PostsAPI
+from .api.publication import PublicationsAPI
+from .api.user import UserAPI
 from .auth import SubstackAuth
 from .utils.http import HTTPClient
 
@@ -7,13 +10,16 @@ class SubstackClient:
         self._substack_domain = substack_domain
         self._auth = SubstackAuth(substack_domain)
         self._http_client = HTTPClient(substack_domain, self._auth)
+        self.posts: PostsAPI = PostsAPI(self._http_client, self._substack_domain)
+        self.publications: PublicationsAPI = PublicationsAPI(self._http_client, self._substack_domain)
+        self.user: UserAPI = UserAPI(self._http_client, self._substack_domain)
 
     @property
     def substack_domain(self) -> str:
         return self._substack_domain
 
     def login(self, email: str, password: str) -> bool:
-        return self._auth.login(self._http_client, email, password)
+        return self._auth.login(self._http_client, email, password, self._substack_domain)
 
     def set_auth_cookie(self, auth_cookie: str) -> None:
         self._auth.set_auth_cookie_manually(auth_cookie)
